ec0c514aae8194c5abcb6bc14ec4f40d9e753b91
Methewdev <envidevelopment@gmail.com>
2026-06-27 14:25:27 +0700

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -320,15 +320,15 @@ with tab1:
     # PREVIEW DATASET
     # =====================================================
 
-        st.subheader("📄 Preview Dataset")
+    st.subheader("📄 Preview Dataset")
 
-        preview_df = df.copy()
-        preview_df.index = range(1, len(preview_df) + 1)
+    preview_df = df.copy()
+    preview_df.index = range(1, len(preview_df) + 1)
 
-        st.dataframe(
-        preview_df,
-        width="stretch"
-        )
+    st.dataframe(
+    preview_df,
+    width="stretch"
+    )
     # =====================================================
     # MISSING VALUE
     # =====================================================
