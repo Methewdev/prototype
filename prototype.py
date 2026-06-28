b0f3d9291fa07873a0df60a0287e5693b2541712
Methewdev <envidevelopment@gmail.com>
2026-06-28 15:20:09 +0700

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -1048,17 +1048,17 @@ with tab9:
         process_df = st.session_state.processed_df
 
         preview = process_df[
-    [
-        "final_text",
-        "teacher_sentiment",
-        "sentiment_score"
-    ]
+            [
+                "final_text",
+                "teacher_sentiment",
+                "sentiment_score"
+            ]
     ].copy()
 
         preview.insert(
-    0,
-    "No",
-    range(1, len(preview) + 1)
+            0,
+            "No",
+            range(1, len(preview) + 1)
     )
 
     st.dataframe(
